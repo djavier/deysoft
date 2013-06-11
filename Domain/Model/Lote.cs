07ab42c9ae717ff468f3ae09f676adc452e5a745
djavier <dariel.javier@theberrycompany.com>
2013-06-11 08:27:33 -0400

    Start Lote Repositories

---
+++ Domain/Model/Lote.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain.Model
+{
+  public partial class Lote
+  {
+    public Lote()
+    {
+      Id = Guid.NewGuid();
+    }
+  }
+}
---
+++ Domain/Repositories/LoteRepository.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Domain.Model;
+using System.Transactions;
+
+namespace Domain.Repositories
+{
+  public class LoteRepository : IRepository<Lote>
+  {
+    #region ILoteRepository Members
+
+   public void Save(Lote entity)
+    {
+      using (var session = new DEYSoftEntities())
+      {
+        using (var tran = new TransactionScope())
+        {
+          try
+          {
+            session.Lotes.AddObject(entity);
+            session.ObjectStateManager.ChangeObjectState(entity, System.Data.EntityState.Added);
+            session.SaveChanges();
+            tran.Complete();
+          }
+          catch (Exception ex)
+          {
+            throw ex;
+          }
+        }
+      }
+    }
+
+   public void Update(Lote entity)
+    {
+      using (var session = new DEYSoftEntities())
+      {
+        using (var tran = new TransactionScope())
+        {
+          session.Lotes.Attach(entity);
+          session.ObjectStateManager.ChangeObjectState(entity, System.Data.EntityState.Modified);
+          session.SaveChanges();
+          tran.Complete();
+        }
+      }
+    }
+
+
+
+   public Lote GetById(Guid id)
+    {
+      using (var session = new DEYSoftEntities())
+        return session.Lotes.Where(x => x.Id == id).FirstOrDefault();
+    }
+
+   public IList<Lote> GetAll()
+    {
+      using (var session = new DEYSoftEntities())
+        return session.Lotes.ToList();
+    }
+
+   public void Delete(Lote entity)
+    {
+      using (var session = new DEYSoftEntities())
+      {
+        using (var tran = new TransactionScope())
+        {
+          session.DeleteObject(entity);
+          session.ObjectStateManager.ChangeObjectState(entity, System.Data.EntityState.Deleted);
+          session.SaveChanges();
+          tran.Complete();
+        }
+      }
+    }
+
+   public void Delete(Guid id)
+   {
+     using (var session = new DEYSoftEntities())
+     {
+       using (var tran = new TransactionScope())
+       {
+         session.DeleteObject(this.GetById(id));
+         session.SaveChanges();
+         tran.Complete();
+       }
+     }
+   }
+
+  
+    #endregion
+  }
+}
